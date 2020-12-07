c3fe0edb298261d98bb961a0dee2f94aa8cae408
prabhu-sunderaraman <prabhu.bits@gmail.com>
2020-12-07 12:15:55 +0530

    Day01

--- Day01/src/com/lab02/Lab02Main.java
+++ Day01/src/com/lab02/Lab02Main.java
@@ -5,14 +5,14 @@ public class Lab02Main {
 
 	public static void main(String[] args) {
 		String userName = "samy123";
-		final boolean userNameValid = isNotBlank
-			.and(isNotNull.and(isUsernameLengthBw5And10))
+		final boolean userNameValid = isNotNull
+			.and(isNotBlank.and(isUsernameLengthBw5And10))
 			.test(userName);
 		System.out.println(userNameValid);
 		
 		String password = "23o8476";
-		final boolean passwordValid = isNotBlank
-			.and(isNotNull.and(isPasswordLengthBw3And8))
+		final boolean passwordValid = isNotNull
+			.and(isNotBlank.and(isPasswordLengthBw3And8))
 			.test(password);
 		System.out.println(passwordValid);
 		
